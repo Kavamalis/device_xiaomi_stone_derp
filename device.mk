1cabd3837e1c93ca2cf5ceec0fd5b0be9523011e
baunilla <baunilla@tuta.io>
2024-08-22 19:29:26 +0100

    stone: gps: Update GPS configs from stock
    * From sunstone:14/UKQ1.230917.001/V816.0.7.0.UMQMIXM

--- device.mk
+++ device.mk
@@ -243,11 +243,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
-# GPS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@2.1.vendor
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1.vendor \
