e43ca3f953bfb37789d342ccd5d79ec7cfc36aaa
Khnome <75486623+Khnome@users.noreply.github.com>
2025-03-08 17:28:10 +0200

    stone: come back to old extracting mechanism
    Co-authored-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,51 +1,15 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-DEVICE=stone
-VENDOR=xiaomi
+export DEVICE=stone
+export DEVICE_COMMON=sm6375-common
+export VENDOR=xiaomi
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-function vendor_imports() {
-    cat <<EOF >>"$1"
-        "hardware/qcom/display",
-        "hardware/qcom/display/gralloc",
-        "hardware/qcom/display/libdebug",
-        "hardware/qcom/display/sde-drm",
-        "hardware/xiaomi",
-        "vendor/qcom/common/vendor/display",
-        "vendor/qcom/common/vendor/display/5.4",
-        "vendor/qcom/common/vendor/gps-legacy",
-        "vendor/qcom/common/vendor/media-legacy",
-        "vendor/qcom/common/vendor/wlan-legacy",
-EOF
-}
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
-
-# Warning headers and guards
-write_headers
-
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-
-# Finish
-write_footers
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
