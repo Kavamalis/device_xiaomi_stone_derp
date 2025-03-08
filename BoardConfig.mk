a1c815221106df09e9cc0ffc56d05e5057ea5808
Khnome <75486623+Khnome@users.noreply.github.com>
2025-03-08 10:52:04 +0200

    stone: device.mk: add sepolicy make files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,4 +58,8 @@ TARGET_OTA_ASSERT_DEVICE := moonstone,sunstone
 include vendor/xiaomi/stone/BoardConfigVendor.mk
 
 # Inherit from sm6375-common
-include device/xiaomi/sm6375-common/BoardConfigCommon.mk
+include device/xiaomi/sm6375/BoardConfigCommon.mk
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
