e4aa17f81c9a938f068ebc2debaa497d9df27c05
Khnome <75486623+Khnome@users.noreply.github.com>
2025-04-17 19:41:13 +0300

    Revert "stone: Move recovery resources to vendor_boot image"
    This reverts commit 0c62ffb31843b530b861a39b2ece9c0f7edcc424.

--- device.mk
+++ device.mk
@@ -16,9 +16,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-# A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Device uses high-density artwork where available
