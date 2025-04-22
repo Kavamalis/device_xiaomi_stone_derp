19edf0102b5ac639f3cf400b217b26766ba4f49a
Khnome <75486623+Khnome@users.noreply.github.com>
2025-04-22 07:20:08 +0300

    Revert "stone: switch to inline kernel building"
    This reverts commit 6e478997cf37cd792a6c29d83c61eee30c4d9901.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,26 +110,43 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
 
-BOARD_KERNEL_CMDLINE := \
-    androidboot.hardware=qcom \
-    androidboot.memcg=1 \
-    lpm_levels.sleep_disabled=1 \
-    msm_rtb.filter=0x237 \
-    service_locator.enable=1 \
-    androidboot.usbcontroller=4e00000.dwc3 \
-    swiotlb=0 \
-    loop.max_part=7 \
-    cgroup.memory=nokmem,nosocket \
-    iptable_raw.raw_before_defrag=1 \
-    ip6table_raw.raw_before_defrag=1 \
-    firmware_class.path=/vendor/firmware
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=4e00000.dwc3
+BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += swiotlb=0
+BOARD_KERNEL_CMDLINE += pcie_ports=compat
+BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
-TARGET_KERNEL_CONFIG := stone_defconfig
+# Prebuilt Kernel
+ifeq ($(PREBUILT_KERNEL),true)
+BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL := false
+TARGET_KERNEL_SOURCE := $(DEVICE_PATH)-kernel/kernel-headers
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)-kernel/dtbo.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)-kernel/kernel
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)-kernel/dtb.img
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)-kernel/dtb.img:$(TARGET_COPY_OUT)/dtb.img \
+    $(DEVICE_PATH)-kernel/kernel:kernel
+else
 TARGET_KERNEL_SOURCE := kernel/xiaomi/stone
+TARGET_KERNEL_CONFIG := stone_defconfig
 TARGET_KERNEL_NO_GCC := true
-TARGET_KERNEL_VERSION := 5.4
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+endif
 
-# OTA assert
+# Media
+TARGET_USES_ION := true
+TARGET_DISABLED_UBWC := true
+
+# OTA Assert
 TARGET_OTA_ASSERT_DEVICE := moonstone,sunstone,stone
 
 # Partitions
