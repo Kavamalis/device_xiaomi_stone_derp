edbb45c9a511c08608904feb5f02bd3bd053e342
Khnome <75486623+Khnome@users.noreply.github.com>
2025-04-29 21:42:44 +0300

    Revert "stone: add oneplus dolby"
    This reverts commit 06320454f26f0ec7874b4a3ba6c97144cd285d9a.

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # Call the ViperFX Config
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
-# Vendor Dolby
-$(call inherit-product, vendor/dolby/dolby.mk)
-
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
