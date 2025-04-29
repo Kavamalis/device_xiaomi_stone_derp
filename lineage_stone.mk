8fd26ea3bcf8e877a0d077e7885a834db304aff2
Khnome <75486623+Khnome@users.noreply.github.com>
2025-04-29 21:18:51 +0300

    stone: DerpFest lineage bringup

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -9,20 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Derp stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
 PREBUILT_KERNEL := true
 
-PRODUCT_NAME := derp_stone
+PRODUCT_NAME := lineage_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X5 5G
-
-DERP_BUILDTYPE := Unofficial
+DERPFEST_BUILD_TYPE := Community
 
 include vendor/lineage-priv/keys/keys.mk
 
